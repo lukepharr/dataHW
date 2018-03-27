c0033716131e128f5fcb610469c8459d18eef614
Luke Pharr <Luke@lukes-mbp-2.attlocal.net>
2018-03-27 10:33:15 -0500

    started hw

---
+++ HW8/HW8.sql
@@ -0,0 +1,8 @@
+Use Sakila;
+#1a
+select first_name, last_name from actor;
+#1b
+select concat(first_name, ' ', last_name) from actor;
+#2a
+select first_name, last_name, actor_id from actor
+where first_name = 'Joe';

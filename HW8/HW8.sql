cde5789db21ae5f3762d4f9b4b0e0bae8f58c2f0
Luke Pharr <Luke@lukes-mbp-2.attlocal.net>
2018-03-27 10:45:01 -0500

    going well

--- HW8/HW8.sql
+++ HW8/HW8.sql
@@ -9,3 +9,10 @@ where first_name = 'Joe';
 #2b
 select first_name, last_name, actor_id from actor
 where last_name like '%gen%';
+#2c
+select first_name, last_name, actor_id from actor
+where last_name like '%li%' 
+order by last_name, first_name;
+#2d 
+select country_id, country from country
+where country in ('Afghanistan','Bangladesh','China');

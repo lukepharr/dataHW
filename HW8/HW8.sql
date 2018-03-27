3d9f8f2d6ee37c92d31115093b743612d3689068
Luke Pharr <Luke@lukes-mbp-2.attlocal.net>
2018-03-27 10:37:36 -0500

    started hw

--- HW8/HW8.sql
+++ HW8/HW8.sql
@@ -6,3 +6,6 @@ select concat(first_name, ' ', last_name) from actor;
 #2a
 select first_name, last_name, actor_id from actor
 where first_name = 'Joe';
+#2b
+select first_name, last_name, actor_id from actor
+where last_name like '%gen%';

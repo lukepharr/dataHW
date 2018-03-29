6fd8813b0957c7204255a2b3ceee29cb935db514
Luke Pharr <Luke@lukes-mbp-2.attlocal.net>
2018-03-29 13:00:27 -0500

    on 6

--- HW8/HW8.sql
+++ HW8/HW8.sql
@@ -36,6 +36,19 @@ update actor set first_name = 'Harpo' where first_name = 'Groucho' and last_name
 update actor 
 set first_name = case when first_name='Harpo' then 'Groucho' else 'Mucho Groucho' end
 where (actor_id=172);
+#5a
+show create table address;
+#result: CREATE TABLE `address` (\n  `address_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,\n  `address` varchar(50) NOT NULL,\n  `address2` varchar(50) DEFAULT NULL,\n  `district` varchar(20) NOT NULL,\n  `city_id` smallint(5) unsigned NOT NULL,\n  `postal_code` varchar(10) DEFAULT NULL,\n  `phone` varchar(20) NOT NULL,\n  `location` geometry NOT NULL,\n  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\n  PRIMARY KEY (`address_id`),\n  KEY `idx_fk_city_id` (`city_id`),\n  SPATIAL KEY `idx_location` (`location`),\n  CONSTRAINT `fk_address_city` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`) ON UPDATE CASCADE\n) ENGINE=InnoDB AUTO_INCREMENT=606 DEFAULT CHARSET=utf8
+#6a
+select staff.first_name, staff.last_name, address.address
+from staff
+inner join address
+on staff.address_id=address.address_id;
+#6b
+select staff.first_name, staff.last_name, sum(payment.amount)
+from staff
+inner join payment
+on staff.staff_id=payment.staff_id
+where month(payment.payment_date)=8 and year(payment.payment_date)=2005
+group by first_name, last_name;
 
-
-172

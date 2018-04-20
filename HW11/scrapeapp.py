7b8edef3f22d0cc9f3c6d29a8a13702d05d8330f
Luke Pharr <Luke@Lukes-MacBook-Pro-2.local>
2018-04-20 10:39:15 -0500

    break

--- HW11/scrapeapp.py
+++ HW11/scrapeapp.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 import pymongo
+import scrape
 
 conn="mongodb://localhost/27017"
 client = pymongo.MongoClient(conn)
 db=client.MarsDB
+collection=db.marsdata
 # @TODO: Initialize your Flask app here
 # CODE GOES HERE
 
@@ -13,8 +15,14 @@ app = Flask(__name__)
 
 @app.route('/scrape')
 def scrape():
-	import scrape
-	db.MarsDB.insert(scrape_nasa)
+	data=scrape.scrape_nasa
+	db.collection.insert(data)
+	return render_template('scrape.html')
+
+@app.route('/')
+def db_insert():
+	data_=db.collection.find()[0]
+	return render_template('index.html', data=data_)
 
 if __name__ == "__main__":
     app.run(debug=True)

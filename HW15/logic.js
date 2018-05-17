54ba758c8f087cf8951df367d20a9356738b911f
Luke Pharr <Luke@lukes-mbp-2.attlocal.net>
2018-05-17 14:09:32 -0500

    first commit

--- HW15/logic.js
+++ HW15/logic.js
@@ -8,12 +8,49 @@ L.tileLayer(
     "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?" +
       "access_token=pk.eyJ1IjoibHVrZXBoYXJyIiwiYSI6ImNqZ3liOWxhcjBjYWwzM21pbmJsaGt5OTcifQ.YBqhxW8Hg0S93a42LaVWZw"
   ).addTo(myMap);
-
+function getColor(d) {
+    return d > 9 ? '#800026' :
+           d > 7  ? '#BD0026' :
+           d > 5  ? '#E31A1C' :
+           d > 4  ? '#FC4E2A' :
+           d > 3   ? '#FD8D3C' :
+           d > 2   ? '#FEB24C' :
+           d > 1   ? '#FED976' :
+                      '#FFEDA0';
+}
 d3.json(url, function(response){
 	// console.log(response.features)
 	response.features.forEach(function(d){
-		L.circle(d.geometry.coordinates.slice(0,2))
-		// console.log(d.geometry.coordinates.slice(0,2))
-		
+		var mag = Number(d.properties.mag)
+		L.circle([d.geometry.coordinates[1],d.geometry.coordinates[0]],
+			{
+				color: getColor(mag),
+				fillOpacity: .5,
+				radius: mag*30000
+			}).addTo(myMap)
+
 	})
- })
+});
+
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+    mags = [0, 1, 2, 3, 4, 5, 7, 9],
+    labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < mags.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(mags[i]+1) + '"></i> ' +
+            mags[i] + (mags[i + 1] ? '&ndash;' + mags[i + 1] + '<br>' : '+');
+}
+
+return div;
+};
+
+legend.addTo(myMap);
+
+
+
+
